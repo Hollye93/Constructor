cf0966de372df7ae1befe3d9d112f4d98b6081f2
Hollye93 <hhamilton@prestigestaffing.com>
2018-03-04 15:02:05 -0500

    updates!

---
+++ game.js
@@ -0,0 +1,27 @@
+// Create a list of words
+var listOfWords = [
+  'html',
+  'css',
+  'javascript',
+  'jquery',
+  'ajax',
+  'node',
+  'ruby',
+  'c',
+  'php',
+  'python',
+  'java',
+  'sql',
+  'mysql',
+  'mongo',
+  'matlab',
+  'firebase',
+  'mongoose',
+  'express',
+  'react',
+  'meteor',
+  'handlebars'
+];
+
+// Export the list of words to other hangman script(s)
+module.exports = listOfWords;
